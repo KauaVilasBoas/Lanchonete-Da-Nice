5ba92dcd050007eaa93d509c1ca5e01cad22fde3
KauaVilasBoas <kauacaldeira@hotmail.com>
2023-08-11 13:02:51 -0300

    chore: cliente.html

--- src/main/resources/static/cliente/js/script.js
+++ src/main/resources/static/cliente/js/script.js
@@ -62,8 +62,9 @@
                     <td>${cliente.email}</td>
                     <td>${cliente.telefone}</td>
                     <td>
-                     <button class="btn btn-primary mr-2" onclick="editarCliente(${cliente.id})">Editar</button>
+                     <button class="btn btn-primary mr-2" onclick="abrirModalEditarCliente(${cliente.id})">Editar</button>
                         <button class="btn btn-danger" onclick="excluirCliente(${cliente.id})">Excluir</button>
+                        <button class="btn btn-primary mr-2" onclick="abrirModalDetalharCliente(${cliente.id})">Detalhar</button>
                     </td>
                 `;
                     tbody.appendChild(tr);
@@ -78,31 +79,29 @@
             });
     }
 
-    function editarCliente(clienteId) {
+    function abrirModalEditarCliente(clienteId) {
         fetch(`http://localhost:8080/clientes/${clienteId}`)
             .then(response => response.json())
             .then(cliente => {
-                document.getElementById('resultado').innerHTML = `
-                <h2>Editar Cliente</h2>
-                <form id="formEditar">
-                    <div class="mb-3">
-                        <label for="nomeEditar" class="form-label">Nome:</label>
-                        <input type="text" class="form-control" id="nomeEditar" value="${cliente.nome}">
-                    </div>
-                    
-                    <div class="mb-3">
-                        <label for="telefoneEditar" class="form-label">Telefone:</label>
-                        <input type="tel" class="form-control" id="telefoneEditar" value="${cliente.telefone}" pattern="[0-9]{10,11}">
-                    </div>
-                    
-                    <button type="submit" class="btn btn-primary">Salvar</button>
-                </form>
+                const modal = new bootstrap.Modal(document.getElementById('modalEditarCliente'));
+                const form = document.getElementById('formEditarCliente');
+
+                form.innerHTML = `
+                <input type="hidden" id="clienteIdEditar" value="${cliente.id}">
+                
+                <div class="mb-3">
+                    <label for="nomeEditar" class="form-label">Nome:</label>
+                    <input type="text" class="form-control" id="nomeEditar" value="${cliente.nome}">
+                </div>
+                
+                <div class="mb-3">
+                    <label for="telefoneEditar" class="form-label">Telefone:</label>
+                    <input type="tel" class="form-control" id="telefoneEditar" value="${cliente.telefone}" pattern="[0-9]{10,11}">
+                </div>
             `;
 
-                document.getElementById('formEditar').addEventListener('submit', function(event) {
-                    event.preventDefault();
-
-                    const idEditar = cliente.id; // Obtendo o id do cliente
+                document.getElementById('btnSalvarEdicao').addEventListener('click', function() {
+                    const idEditar = document.getElementById('clienteIdEditar').value;
                     const nomeEditar = document.getElementById('nomeEditar').value;
                     const telefoneEditar = document.getElementById('telefoneEditar').value;
 
@@ -121,7 +120,7 @@
                     })
                         .then(response => {
                             if (response.ok) {
-                                document.getElementById('resultado').innerText = 'Cliente editado com sucesso!';
+                                modal.hide();
                                 listarClientes();
                             } else {
                                 console.error('Erro ao editar cliente.');
@@ -131,11 +130,14 @@
                             console.error('Erro ao editar cliente.', error);
                         });
                 });
+
+                modal.show();
             })
             .catch(error => {
                 console.error('Erro ao obter dados do cliente para edição.', error);
             });
     }
+
 
 
 
@@ -154,3 +156,25 @@
                 console.error('Erro ao excluir cliente.', error);
             });
     }
+
+    function abrirModalDetalharCliente(clienteId) {
+        fetch(`http://localhost:8080/clientes/${clienteId}`)
+            .then(response => response.json())
+            .then(cliente => {
+                const modal = new bootstrap.Modal(document.getElementById('modalDetalharCliente'));
+                const modalBody = document.getElementById('modalDetalharClienteBody');
+
+                modalBody.innerHTML = `
+                <p><strong>Nome:</strong> ${cliente.nome}</p>
+                <p><strong>Email:</strong> ${cliente.email}</p>
+                <p><strong>Telefone:</strong> ${cliente.telefone}</p>
+                <p><strong>CPF:</strong> ${cliente.cpf}</p>
+            `;
+
+                modal.show();
+            })
+            .catch(error => {
+                console.error('Erro ao obter dados do cliente para detalhar.', error);
+            });
+    }
+
